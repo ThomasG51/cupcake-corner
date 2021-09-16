5820d0bc86827d0402c499a3234c7c2122c23c94
Thomas George <private@thomas-george.com>
2021-09-16 06:52:37 +0200

    Initial Commit

---
+++ Project_9_Cupcake_Corner/Project_9_Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_9_Cupcake_CornerApp.swift
+//  Project_9_Cupcake_Corner
+//
+//  Created by Thomas George on 16/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_9_Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
